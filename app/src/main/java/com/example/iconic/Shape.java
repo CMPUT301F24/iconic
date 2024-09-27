df139d3a594fd2586809ae48ec723ab4f5318e7c
AidenTeal <ateal@ualberta.ca>
2024-09-27 10:12:26 -0600

    Change color of shape

--- app/src/main/java/com/example/iconic/Shape.java
+++ app/src/main/java/com/example/iconic/Shape.java
@@ -5,7 +5,6 @@ public abstract class Shape {
     protected Integer y;
 
     // Edit the Shape class by adding a color string field. (String color = "blue";) (local your-own branch)
-    protected String color = "green";
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
